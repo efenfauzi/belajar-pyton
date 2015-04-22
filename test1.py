907781c3af20adf48df8825dafb67601d447eeb8
efen.fauzi <efen.fauzi@gmail.com>
2015-04-22 15:48:45 +0700

    tanda petik

---
+++ test1.py
@@ -0,0 +1,13 @@
+
+print "'haay, sapa namamu?',tanya:jono"
+print '"wooow", kamu suka "dia" ya?'
+
+
+print '"haay, sapa namamu?" ,tanya:jono'
+print "'wooow', kamu suka 'dia' ya?"
+
+#double quote strings dengan single quote strings
+"""
+Tidak ada perbedaan OUtput dalam fungsi petik satu dan petik dua,
+namun yang perlu diperhatikaan adalah penggunaannya dalam sebuah kalimat(strings or letter)
+"""
