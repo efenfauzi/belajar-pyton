6dcc215f24eacb9837d4b3fb5c59be61e32340e9
efen.fauzi <efen.fauzi@gmail.com>
2015-04-22 15:31:43 +0700

    dasar perbedaan shell dengan script

---
+++ test.py
@@ -0,0 +1,11 @@
+'inilah test...' #teks 1
+"inilah test..." #teks 2
+print 'inilah test...' #teks 3
+print "inilah test..." #teks 4 
+
+"""
+Python dapat digunakan dalam mode shell/prompt dan script mode
+teks 1 dan 2 digunakan pada mode prompt , penanda >>> 
+teks 3 dan 4 digunakan pda mode scripting , dipanggil python "nama_file".py
+source : http://www.openbookproject.net/thinkcs/python/english2e/ch01.html
+"""
